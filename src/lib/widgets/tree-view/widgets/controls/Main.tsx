cebbeb4075febb84b556589dfa5e1cca97c818e6
Valentin Kantor <kantorvv@gmail.com>
2024-08-31 16:24:17 +0300

    unused code removed

--- src/lib/widgets/tree-view/widgets/controls/Main.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Typography } from '@mui/material';
-
-const TreeControlsBar = () => (
-  <Box>
-    <Typography variant="button">TreeControlsBar</Typography>
-  </Box>
-);
-
-export { TreeControlsBar };
