f4847852582c19458d42173dcfe8db469c578b83
Valentin Kantor <kantorvv@gmail.com>
2024-10-07 23:20:42 +0300

    hardcoded styles removed

--- src/stories/TreeViewer.expanded.stories.tsx
+++ src/stories/TreeViewer.expanded.stories.tsx
@@ -40,10 +40,9 @@ type TreeViewComponentProps = {
         sx={{
           display: 'flex',
           flexDirection: 'column',
-          background: 'wheat',
           height: '100%',
-          maxWidth: 400,
-          minHeight: 800,
+        //   maxWidth: 400,
+        //   minHeight: 800,
         }}
       >
         <Box
